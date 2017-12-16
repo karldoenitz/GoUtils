ded6612a5d8637820cb8d86f43b2347b9426ca09
karl <karlvorndoenitz@gmail.com>
2017-12-16 17:59:03 +0800

    增加socket模块

--- redisoperator/RedisOperator.go
+++ redisoperator/RedisOperator.go
@@ -1,1 +1,37 @@
 package redisoperator
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
+
+//这是一个go操作redis的简单用例
+func RedisDemo()  {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exists")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+}
