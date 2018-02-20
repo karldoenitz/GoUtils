3d79ff5f8314cc0fe5d4349c6c96ba3fbe5a10a1
lizhihao <karlvorndoenitz@gmail.com>
2018-02-20 23:29:18 +0800

    增加了ip转换数字函数

--- main/main.go
+++ main/main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	ip := "192.168.1.169"
 	number := netutils.IpToNumber(ip)
-	logger.Log.Printf("Server %s pid=%d started with processes: %d", number, os.Getpid(),runtime.GOMAXPROCS(runtime.NumCPU()))
+	logger.Info.Printf("Server %d pid=%d started with processes: %d", number, os.Getpid(),runtime.GOMAXPROCS(runtime.NumCPU()))
     newIp := netutils.NumberToIp(number)
-	logger.Log.Printf("Server %s pid=%d started with processes: %d", newIp, os.Getpid(),runtime.GOMAXPROCS(runtime.NumCPU()))
+	logger.Error.Printf("Server %s pid=%d started with processes: %d", newIp, os.Getpid(),runtime.GOMAXPROCS(runtime.NumCPU()))
 	search.Test_solr()
 }
