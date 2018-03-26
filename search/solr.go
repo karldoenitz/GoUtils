cea01121a426633b0c45629b817b25bfa6466c2b
karl <karlvorndoenitz@gmail.com>
2018-03-26 16:38:17 +0800

    修改solr模块demo

--- search/solr.go
+++ search/solr.go
@@ -3,12 +3,20 @@ package search
 import (
 	"github.com/rtt/Go-Solr"
 	"fmt"
-	"reflect"
+	//"reflect"
 )
 
-const solrHost = "10.98.16.215"
-const port = 9160
-const core = "mitv/search/movie"
+const solrHost = "127.0.0.1"
+const port = 8983
+const core = "music"
+
+type Music struct {
+	id int64
+	name string
+	singer []int64
+	searchWorld []string
+	url string
+}
 
 func Test_solr()  {
 	solrClient, err := solr.Init(solrHost, port, core)
@@ -18,12 +26,12 @@ func Test_solr()  {
 	q := solr.Query{
 		Params: solr.URLParamMap{
 			"q": []string{"*:*"},
-			"fq": []string{"id:123221"},
+			"fq": []string{"id:1"},
 			"wt": []string{"json"},
 			"fl": []string{"*"},
 		},
 		Rows: 10,
-		Sort: "title+ASC",
+		Sort: "name+ASC",
 	}
 	queryString := q.String()
 	fmt.Println(queryString)
@@ -34,13 +42,29 @@ func Test_solr()  {
 	results := res.Results
 	for i := 0; i < results.NumFound; i++ {
 		collection := results.Collection[i]
-		fmt.Println("id:", collection.Fields["id"])
-		fmt.Println("title:", collection.Fields["title"])
-		titleNorm := reflect.ValueOf(collection.Fields["title_norm"])
-		//index := []int{0}
-		fmt.Println("title_norm:", titleNorm.Index(0))
-		//titleNorm := collection.Fields["title_norm"]
-		//fmt.Println("title_norm", titleNorm)
-		fmt.Println("---------------------------------")
+		id := int64(collection.Fields["id"].(float64))
+		name := collection.Fields["name"].(string)
+		url := collection.Fields["url"].(string)
+		singerInterface := collection.Fields["singer"].([]interface{})
+		searchWordInterface := collection.Fields["searchWord"].([]interface{})
+		singer := make([]int64, len(singerInterface))
+		for i := range singerInterface {
+			singer[i] = int64(singerInterface[i].(float64))
+		}
+		searchWord := make([]string, len(searchWordInterface))
+		for i := range searchWordInterface {
+			searchWord[i] = searchWordInterface[i].(string)
+		}
+		fmt.Println(id, name, url, singer, searchWord)
+		music := Music{
+			id: id,
+			name: name,
+			url: url,
+			singer: singer,
+			searchWorld: searchWord,
+		}
+		fmt.Println(music)
+		//titleNorm := reflect.ValueOf(collection.Fields["title_norm"])
+		//fmt.Println("title_norm:", titleNorm.Index(0))
 	}
 }
