8c2795221b2aeabbb234f028a65ef933749b60d5
lizhihao <lizhihao@kuaishou.com>
2020-04-14 16:36:06 +0800

    完成del函数

--- sequenceutils/utils.go
+++ sequenceutils/utils.go
@@ -43,33 +43,77 @@ func IsIn(obj interface{}, val interface{}) (isIn bool) {
 //  - container: 容器型变量的引用，array、slice、map或者string类型
 //  - key: 待删除元素的索引
 func DelFrom(container interface{}, key interface{}) {
-	vField := reflect.ValueOf(container)
-	value := vField.Elem()
-	if value.Kind() == reflect.Slice || value.Kind() == reflect.Array {
+	pField := reflect.ValueOf(container)
+	vField := pField.Elem()
+	if vField.Kind() == reflect.Slice || vField.Kind() == reflect.Array {
 		index := key.(int)
-		if index < 0 || index+1 > value.Len() {
+		if index < 0 || index+1 > vField.Len() {
 			return
 		}
-		result := reflect.AppendSlice(value.Slice(0, index), value.Slice(index+1, value.Len()))
-		value.Set(result)
+		result := reflect.AppendSlice(vField.Slice(0, index), vField.Slice(index+1, vField.Len()))
+		vField.Set(result)
 		return
 	}
-	if value.Kind() == reflect.Map {
-		for _, mapKey := range value.MapKeys() {
+	if vField.Kind() == reflect.Map {
+		for _, mapKey := range vField.MapKeys() {
 			if mapKey.Interface() == key {
-				value.SetMapIndex(mapKey, reflect.Value{})
+				vField.SetMapIndex(mapKey, reflect.Value{})
 			}
 		}
 		return
 	}
-	if value.Kind() == reflect.String {
+	if vField.Kind() == reflect.String {
 		index := key.(int)
-		if index < 0 || index+1 > value.Len() {
+		if index < 0 || index+1 > vField.Len() {
 			return
 		}
 		ps := container.(*string)
 		result := (*ps)[:index] + (*ps)[index+1:]
-		value.SetString(result)
+		vField.SetString(result)
 		return
 	}
+}
+
+// SliceSeq 对有序变量进行切片
+//  - seq: 有序变量，string、array或slice类型
+//  - startIdx: 首位置索引
+//  - endIdx: 末位置索引
+func SliceSeq(seq interface{}, startIdx, endIdx int) (result interface{}) {
+	vField := reflect.ValueOf(seq)
+	var length int
+	vKind := vField.Kind()
+	if vKind == reflect.Array || vKind == reflect.Slice || vKind == reflect.String {
+		length = vField.Len()
+		if startIdx < 0 {
+			startIdx = length + startIdx
+		}
+		if endIdx < 0 {
+			endIdx = length + endIdx
+		}
+		if startIdx < 0 {
+			startIdx = 0
+		}
+		if endIdx > length {
+			endIdx = length
+		}
+	}
+	if vKind == reflect.Array || vKind == reflect.Slice {
+		// 原切片长度小于等于0，或者截取长度小于等于0，则直接返回空切片
+		if length <= 0 || endIdx-startIdx <= 0 {
+			result = reflect.New(reflect.TypeOf(seq)).Elem().Interface()
+			return
+		}
+		result = vField.Slice(startIdx, endIdx).Interface()
+		return
+	}
+	if vKind == reflect.String {
+		if length <= 0 || endIdx-startIdx <= 0 {
+			result = ""
+			return
+		}
+		seqStr := seq.(string)
+		result = seqStr[startIdx:endIdx]
+		return
+	}
+	return
 }
